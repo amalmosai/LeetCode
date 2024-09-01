52ad1f71ce909815fee07231b56ca91ff66fe2db
Amal Moussa <75141244+amalmosai@users.noreply.github.com>
2024-09-01 17:46:03 +0300

    [1662] [Time Beats: 56.32%] [Memory Beats: 23.31%] - LeetPush

---
+++ 1662-Check-If-Two-String-Arrays-are-Equivalent.js
@@ -0,0 +1,8 @@
+/**
+ * @param {string[]} w1
+ * @param {string[]} w2
+ * @return {boolean}
+ */
+var arrayStringsAreEqual = function(w1, w2) {
+    return w1.join("")===w2.join("")?true :false;
+};
